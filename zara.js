7e11b11b24284a99430cff5894a48cba40dcbce4
@VirusFucker <90620251+xmain12@users.noreply.github.com>
2021-10-18 10:44:18 +0530

    Update zara.js

--- zara.js
+++ zara.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by virus fucker
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+94779529221' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'VIRUS FUCKER' : process.env.OWNER_NAME,
 
 };
